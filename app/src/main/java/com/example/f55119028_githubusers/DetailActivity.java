1cc6171f5a7cb9480e1cafe3075071a37da9e3eb
Maulana <maulanaadnan001@gmail.com>
2022-04-25 17:41:55 +0800

    Initial Commit

--- app/src/main/java/com/example/f55119028_githubusers/DetailActivity.java
+++ app/src/main/java/com/example/f55119028_githubusers/DetailActivity.java
@@ -1,6 +1,14 @@
 package com.example.f55119028_githubusers;
 
-public class DetailActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
 }
-
